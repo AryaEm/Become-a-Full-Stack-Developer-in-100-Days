43862f8261f05ca4fe78182b517e2f7ee48cffa8
AryaEm <ary4maulanaa@gmail.com>
2024-07-21 17:00:36 +0700

    Day 6 - HTML JavaScript

--- week1/js/day6.js
+++ week1/js/day6.js
@@ -27,3 +27,26 @@ function Hide() {
         element.innerHTML = "Hello World!";
     }
   }       
+
+  document.getElementById("text1").innerHTML = "My First JavaScript";
+
+  function changesStyle() {
+    document.getElementById("text2").style.fontSize = "20px";
+    document.getElementById("text2").style.color = "white";
+    document.getElementById("text2").style.backgroundColor = "black";
+    document.getElementById("text2").style.padding = "10px";
+    document.getElementById("text2").style.textAlign = "center";
+    document.getElementById("text2").style.borderRadius = "10px";
+    document.getElementById("text2").style.cursor = "pointer";
+    document.getElementById("text2").style.margin = "10px";
+  }
+
+  function image(sw) {
+    var element = document.getElementById("myImage");
+    if (sw == 0) {
+        element.style.backgroundImage = "url('/week1/assets/yor.jpg')";
+    } else if (sw == 1){
+        element.style.backgroundImage = "url('/week1/assets/bg_Vestia-Zeta_01.png')";
+    }
+    // element.style.backgroundSize = "cover";
+  }

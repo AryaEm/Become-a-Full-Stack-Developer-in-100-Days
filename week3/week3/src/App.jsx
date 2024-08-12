ff83c5383ec11b78de360a57987f46e6324acc3e
AryaEm <ary4maulanaa@gmail.com>
2024-08-12 15:02:30 +0700

    Day 14 - function

--- week3/week3/src/App.jsx
+++ week3/week3/src/App.jsx
@@ -1,21 +1,26 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
   return (
     <>
       <div className="container">
         <div className="navbar">
-          <div className="nav">
+          <div className="nav-list">
             <a href="">Home</a>
             <a href="">About</a>
             <a href="">Contact</a>
           </div>
+          
+          <div className="nav-logo">
+            <button id="nav-btn"></button>
+          </div>
         </div>
+
+        <div className="content"></div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

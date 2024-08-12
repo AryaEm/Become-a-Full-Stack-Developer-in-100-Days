badffa0eab7d4bf0030946bd3e4db2ffd54ddcdd
AryaEm <ary4maulanaa@gmail.com>
2024-08-12 20:14:19 +0700

    Day 14 - anonymous function

--- week3/week3/src/App.jsx
+++ week3/week3/src/App.jsx
@@ -1,23 +1,30 @@
 import { useState } from "react";
 import "./App.css";
+import FotoGw from './assets/Aryaem.svg'
 
 function App() {
+  const [showNav, setShowNav] = useState(false);
   return (
     <>
       <div className="container">
-        <div className="navbar">
+        {showNav === true && (
           <div className="nav-list">
             <a href="">Home</a>
             <a href="">About</a>
             <a href="">Contact</a>
           </div>
-          
-          <div className="nav-logo">
-            <button id="nav-btn"></button>
-          </div>
+        )}
+
+        <div className="nav-logo">
+          <button id="nav-btn" onClick={() => setShowNav(!showNav)}></button>
         </div>
 
-        <div className="content"></div>
+        <div className="content">
+          <div className="main-content"></div>
+          <div className="main-img">
+            <img src={FotoGw} alt="" />
+          </div>
+        </div>
       </div>
     </>
   );

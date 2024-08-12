7fcb95b1ab59e7da6679be6d8d6885f494c48b38
AryaEm <ary4maulanaa@gmail.com>
2024-08-12 09:06:50 +0700

    starting react

---
+++ week3/week3/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <div className="navbar">
+          <div className="nav">
+            <a href="">Home</a>
+            <a href="">About</a>
+            <a href="">Contact</a>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App

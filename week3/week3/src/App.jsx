0bce5b1a1dbc51c521dd2ab8a6f365bf40db58c4
AryaEm <ary4maulanaa@gmail.com>
2024-08-13 11:37:03 +0700

    Day 15 - react props

--- week3/week3/src/App.jsx
+++ week3/week3/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import FotoGw from './assets/Aryaem.svg'
+import FotoGw from "./assets/Aryaem.svg";
 
 function App() {
   const [showNav, setShowNav] = useState(false);
@@ -20,10 +20,26 @@ function App() {
         </div>
 
         <div className="content">
-          <div className="main-content"></div>
+          <div className="main-content">
+            <div className="content-wrap">
+              <h2>React js</h2>
+              <p>
+                React adalah libray JavaScript yang digunakan untuk membangun
+                user interface yang interaktif berbasis component. React yang
+                dibuat oleh Facebook dan bersifat open source, sehingga dapat
+                digunakan oleh siapa saja secara gratis.
+              </p>
+              <div className="start-btn">
+                <button>Get Started</button>
+              </div>
+            </div>
+          </div>
           <div className="main-img">
             <img src={FotoGw} alt="" />
           </div>
+
+          <div className="box1"></div>
+          <div className="box2"></div>
         </div>
       </div>
     </>
